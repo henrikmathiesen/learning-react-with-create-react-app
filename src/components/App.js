41b7067d17c1d39ecc93850e2bc86c60ca835205
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-17 19:54:46 +0100

    -design

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return(
+        <div className="App">Hello JavaScript World!</div>
+    );
+}
+
+export default App;
+
+
+
+// This is the App Component, a base component that index.js or routes.js handles.
+// Here we used the functional style for the component, instead of classes,
+// this is preferable when the component is focused on presentation rather than state and logic
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+// This is the application starting point
