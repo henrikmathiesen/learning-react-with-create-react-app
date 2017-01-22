78289ed0dd7c48a058f1ca3e2a307c33a17acb8f
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-22 14:09:44 +0100

    -snapshot testing

--- src/components/HelloWorldWithState/HelloWorldWithState.test.js
+++ src/components/HelloWorldWithState/HelloWorldWithState.test.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import HelloWorldWithState from './HelloWorldWithState';
 
-// Can actually reference the component like this
-describe(HelloWorldWithState, () => {
+// Can actually reference the component like this HelloWorldWithState but this creates noise in test command prompt
+describe('HelloWorldWithState', () => {
 
-    it('Should pass', () => {
-        expect(true).toBe(true);
+    const name = 'Person';
+    const mockRemoveGreeting = jest.fn();
+    const component = shallow(
+        <HelloWorldWithState name={name} removeGreeting={mockRemoveGreeting} />
+    );
+
+    it('Renders and matches our snapshot', () => {
+        const component = renderer.create(
+            <HelloWorldWithState name="Person" />
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
     });
 
 });
@@ -15,3 +27,5 @@ describe(HelloWorldWithState, () => {
 // react-test-renderer: Render components to pure JavaScript objects, without depending on the DOM or a native mobile environment
 // enzyme: More flexibility, like shallow rendering (test components in isolation without children)
 // react-addons-test-utils: enzyme requires this package
+
+// snapshot testing: https://facebook.github.io/jest/blog/2016/07/27/jest-14.html
