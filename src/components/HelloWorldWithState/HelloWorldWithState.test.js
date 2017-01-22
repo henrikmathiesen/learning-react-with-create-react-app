9c47c00ec557f4bf37a07d5cbf5f9b2b4b0ac974
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-22 13:14:40 +0100

    -test begun

---
+++ src/components/HelloWorldWithState/HelloWorldWithState.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HelloWorldWithState from './HelloWorldWithState';
+
+// Can actually reference the component like this
+describe(HelloWorldWithState, () => {
+
+    it('Should pass', () => {
+        expect(true).toBe(true);
+    });
+
+});
