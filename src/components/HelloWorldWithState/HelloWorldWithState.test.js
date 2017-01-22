7d841a9d0cca7d02817d9c92f524e3b655ccf20d
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-22 14:46:14 +0100

    -comments

--- src/components/HelloWorldWithState/HelloWorldWithState.test.js
+++ src/components/HelloWorldWithState/HelloWorldWithState.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import HelloWorldWithState from './HelloWorldWithState';
 
-// Can actually reference the component like this HelloWorldWithState but this creates noise in test command prompt
+// Can actually reference the component like this HelloWorldWithState (without quates) but this creates noise in test command prompt
 describe('HelloWorldWithState', () => {
 
     const name = 'Person';
@@ -45,9 +45,17 @@ describe('HelloWorldWithState', () => {
 
 
 // We installed some new packages for this (npm throwed error, needed to do npm cache clean / run bash as admin)
-// react-test-renderer: Render components to pure JavaScript objects, without depending on the DOM or a native mobile environment
-// enzyme: More flexibility, like shallow rendering (test components in isolation without children), also mimicking jQuery's API for DOM manipulation and traversal.
-// react-addons-test-utils: enzyme requires this package
+
+// react-test-renderer
+// - Render components to pure JavaScript objects, without depending on the DOM or a native mobile environment
+// - We can create a JSON from it and make a snapshot for snapshot testing
+
+// enzyme
+// - More flexibility, like shallow rendering (test components in isolation without children)
+// - Also mimicking jQuery's API for DOM manipulation and traversal
+
+// react-addons-test-utils
+// - enzyme requires this package
 
 // snapshot testing: https://facebook.github.io/jest/blog/2016/07/27/jest-14.html
 // press u in test runner to update snap shot if change was intentional
