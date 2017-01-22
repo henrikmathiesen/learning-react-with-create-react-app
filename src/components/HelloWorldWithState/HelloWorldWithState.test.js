34a044a184ecde11f27a1c59ec419ae7a999842a
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-22 13:44:26 +0100

    -installed testing packages

--- src/components/HelloWorldWithState/HelloWorldWithState.test.js
+++ src/components/HelloWorldWithState/HelloWorldWithState.test.js
@@ -9,3 +9,9 @@ describe(HelloWorldWithState, () => {
     });
 
 });
+
+
+// We installed some new packages for this (npm throwed error, needed to do npm cache clean / run bash as admin)
+// react-test-renderer: Render components to pure JavaScript objects, without depending on the DOM or a native mobile environment
+// enzyme: More flexibility, like shallow rendering (test components in isolation without children)
+// react-addons-test-utils: enzyme requires this package
