5528770afe32c59f1ba33efd9f226fb18abe085e
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-23 19:45:47 +0100

    -unit test begun

---
+++ src/components/HelloWorldList/HelloWorldList.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import HelloWorldList from './HelloWorldList';
+import AddGreeter from '../AddGreeter/AddGreeter';
+import HelloWorldWithState from '../HelloWorldWithState/HelloWorldWithState';
+
+describe('test', () => {
+
+    it('Should work', () => {
+        expect(true).toBe(true);
+    });
+
+});
