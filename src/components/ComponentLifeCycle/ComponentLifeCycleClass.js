6bcea96aeb0890040bf3b4b2f6247a37472f7514
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-02-02 21:11:23 +0100

    -comments

--- src/components/ComponentLifeCycle/ComponentLifeCycleClass.js
+++ src/components/ComponentLifeCycle/ComponentLifeCycleClass.js
@@ -7,17 +7,10 @@ class ComponentLifeCycleClass extends Component {
         super(props);
         this.state = { foo: 'bar' };
         console.log("Life cycle 1): constructor");
-
-        // "Don't execute side effects in constructor. It's only for initializing state (and perhaps other variables). Don't make ajax(?) calls or change the state of your app from there."
     }
 
     componentWillMount() {
         console.log("Life cycle 2): componentWillMount");
-
-        // "componentWillMount is rarely useful"
-        // "ES6 class constructor is equivalent of componentWillMount"
-        // "There is talk about removing componentWillMount from class components since it serves the same purpose as the constructor."
-        // http://stackoverflow.com/questions/27139366/why-do-the-react-docs-recommend-doing-ajax-in-componentdidmount-not-componentwi
     }
 
     render() {
@@ -30,8 +23,23 @@ class ComponentLifeCycleClass extends Component {
 
     componentDidMount() {
         console.log("Life cycle 4): componentDidMount");
-        // "If your initialization depends upon the DOM, use componentDidMount, otherwise use constructor"
     }
 }
 
 export default ComponentLifeCycleClass;
+
+
+// Constructor
+// - "Don't execute side effects in constructor. It's only for initializing state (and perhaps other variables). Don't make async calls or change the state of your app from there."
+
+// componentWillMount
+// - "Rarely useful"
+// - "ES6 class constructor is equivalent of componentWillMount"
+// - "There is talk about removing componentWillMount from class components since it serves the same purpose as the constructor."
+
+// componentDidMount
+// - "If your initialization depends upon the DOM, use componentDidMount, otherwise use constructor"
+// - "Make ajax calls in this life cycle hook"
+
+// http://stackoverflow.com/questions/29899116/what-is-the-difference-between-componentwillmount-and-componentdidmount-in-react
+// http://stackoverflow.com/questions/27139366/why-do-the-react-docs-recommend-doing-ajax-in-componentdidmount-not-componentwi
