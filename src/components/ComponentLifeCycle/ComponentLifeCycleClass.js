af1667ac63946113fc00a13d54345f9cfd273314
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-02-03 21:46:22 +0100

    -testing life cycle function

--- src/components/ComponentLifeCycle/ComponentLifeCycleClass.js
+++ src/components/ComponentLifeCycle/ComponentLifeCycleClass.js
@@ -3,18 +3,20 @@ import './ComponentLifeCycle.css';
 
 class ComponentLifeCycleClass extends Component {
 
+    // # MOUNTING
+
     constructor(props) {
         super(props);
         this.state = { foo: 'bar' };
-        console.log("Life cycle 1): constructor");
+        console.log("Life cycle, Mounting 1): constructor");
     }
 
     componentWillMount() {
-        console.log("Life cycle 2): componentWillMount");
+        console.log("Life cycle, Mounting 2): componentWillMount");
     }
 
     render() {
-        console.log("Life cycle 3): Render");
+        console.log("Life cycle, Mounting 3): Render");
 
         return (
             <div className="ComponentLifeCycle">{this.props.message}</div>
@@ -22,12 +24,41 @@ class ComponentLifeCycleClass extends Component {
     }
 
     componentDidMount() {
-        console.log("Life cycle 4): componentDidMount");
+        console.log("Life cycle, Mounting 4): componentDidMount");
+    }
+
+    // # UPDATING
+
+    componentWillReceiveProps() {
+        console.log("Life cycle, Updating: componentWillReceiveProps");
+    }
+
+    shouldComponentUpdate() {
+        console.log("Life cycle, Updating: shouldComponentUpdate");
+    }
+
+    componentWillUpdate() {
+        console.log("Life cycle, Updating: componentWillUpdate");
+    }
+
+    // render(){}
+
+    componentDidUpdate() {
+        console.log("Life cycle, Updating: componentDidUpdate");
+    }
+
+    // # UNMOUNTING
+
+    componentWillUnmount() { 
+        console.log("Life cycle, Unmounting: componentWillUnmount");
     }
 }
 
 export default ComponentLifeCycleClass;
 
+
+// # MOUNTING
+// These methods are called when an instance of a component is being created and inserted into the DOM
 
 // Constructor
 // - "Don't execute side effects in constructor. It's only for initializing state (and perhaps other variables). Don't make async calls or change the state of your app from there."
@@ -43,3 +74,11 @@ export default ComponentLifeCycleClass;
 
 // http://stackoverflow.com/questions/29899116/what-is-the-difference-between-componentwillmount-and-componentdidmount-in-react
 // http://stackoverflow.com/questions/27139366/why-do-the-react-docs-recommend-doing-ajax-in-componentdidmount-not-componentwi
+
+// # UPDATING
+// An update can be caused by changes to props or state. These methods are called when a component is being re-rendered
+
+// Non of the Updating functions were called on the initial "load"
+
+// # UNMOUNTING
+// This method is called when a component is being removed from the DOM
