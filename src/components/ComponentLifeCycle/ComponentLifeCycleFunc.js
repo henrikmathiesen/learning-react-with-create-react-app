b542b995254665bdfeefa5b44904f87860238de9
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-02-05 12:46:38 +0100

    -comments

--- src/components/ComponentLifeCycle/ComponentLifeCycleFunc.js
+++ src/components/ComponentLifeCycle/ComponentLifeCycleFunc.js
@@ -17,4 +17,5 @@ ComponentLifeCycleFunc.propTypes = {
 
 export default ComponentLifeCycleFunc;
 
-// "There are currently no special optimizations done for component functions, although we might add such optimizations in the future. But for now, they perform exactly as classes.""
+// function style components: "These components must not retain internal state, do not have backing instances, and do not have the component lifecycle methods"
+// "There are currently no special optimizations done for component functions, although we might add such optimizations in the future. But for now, they perform exactly as classes."
